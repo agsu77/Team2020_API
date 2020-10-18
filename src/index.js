a3f1a40a640d48496412174f72c917b9741b6f8d
Fitu <f.iturrarte@hotmail.com>
2020-10-18 19:09:17 -0300

    Endpoint de user

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 require('./config/config.js');
 require('./config/connectionBD.js');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+//Routes
+require("./routes/userRoutes")(app);
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor escuchando el ', process.env.PORT);
